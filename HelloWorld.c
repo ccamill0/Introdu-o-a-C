6704af77a1ec7aec02f0068359964f9e8d2a167c
ccamill0 <ccamillo.silva@usp.br>
2024-06-06 14:56:07 -0300

    Add files via upload

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include <stdio.h> //biblioteca necessária para entrada e saída de dados
+
+int main(void) // função principal
+{//ínicio
+    printf("Olá, Mundo!\n\n\n"); //programa pula trê linhas entre o que será impresso e o cursor do terminal
+
+
+    return 0; //valor de retorno da função main - zero significa que executou com sucesso
+
+}//fim do programa
