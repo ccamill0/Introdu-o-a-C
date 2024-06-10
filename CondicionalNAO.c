a5df840a01cb8767b9048fdbc6e9263f7070523e
Kayke <ccamillo.silva@usp.br>
2024-06-10 16:51:26 -0300

    Add files via upload

---
+++ CondicionalNAO.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    float temperatura_max = 36.5, temperatura_min = 15.5,temperatura;
+
+    printf("Digite a temperatura da sala neste momento: ");
+    scanf("%f",&temperatura);
+
+
+    if(!(temperatura < temperatura_min || temperatura > temperatura_max))
+    {
+        printf("Sala está dentro do limite padrão de temperatura\n");
+    }
+
+    else{
+        printf("SALA ESTÁ FORA DO LIMITE PADRÃO E DEVE SER EVACOADA IMEDIATAMENTE!!!!\n");
+    }
+
+    return 0;
+}
